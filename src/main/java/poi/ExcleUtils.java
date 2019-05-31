7bc6d31ce4c7fdb40213f743797237336d2c67a4
zhangjie <2099487761@qq.com>
2019-05-31 16:24:04 +0800

    把excle表格数据读成对象的形式

--- src/main/java/poi/ExcleUtils.java
+++ src/main/java/poi/ExcleUtils.java
@@ -1,4 +1,96 @@
 package poi;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import poi.pojo.CarBaseInfo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExcleUtils {
+
+
+
+    public  static void createSheet(){
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet("新建sheet1");
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("第一個cell");
+        try {
+            FileOutputStream fos = new FileOutputStream("D:\\工作簿.xlsx");
+             workbook.write(fos);
+             fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> readExcleCarData(String filePath){
+        List<CarBaseInfo> carBaseInfoList = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            Workbook workbook = new XSSFWorkbook(fis);
+
+            Sheet sheetAt = workbook.getSheetAt(0);
+
+            int lastRowNum = sheetAt.getPhysicalNumberOfRows();
+
+            Row topRow = sheetAt.getRow(0);
+            int cellNum = topRow.getPhysicalNumberOfCells();
+            for(int i=1;i<lastRowNum;i++){
+                CarBaseInfo carBaseInfo = new CarBaseInfo();
+                Row row = sheetAt.getRow(i);
+                for(int j=0;j<cellNum;j++){
+                    setValues(topRow.getCell(j),row.getCell(j),carBaseInfo);
+                }
+                carBaseInfoList.add(carBaseInfo);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void setValues(Cell topCell, Cell cell, CarBaseInfo carBaseInfo) {
+        String topCellValue = topCell.getStringCellValue();
+        String stringCellValue = null;
+        if (cell !=null){
+             stringCellValue = cell.getStringCellValue();
+        }
+        Field[] declaredFields = carBaseInfo.getClass().getDeclaredFields();
+        for(int i=0;i<declaredFields.length;i++){
+            String name = declaredFields[i].getName();
+            if (topCell.equals(name)){
+                try {
+                    String methodName = "set"+name.substring(0,1).toUpperCase()+name.substring(1);
+                    Method method = carBaseInfo.getClass().getDeclaredMethod(methodName,declaredFields[i].getType());
+                    method.invoke(carBaseInfo,stringCellValue);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+       /*ExcleUtils.createSheet();
+        String s= "vinlist";
+        System.out.println(s.substring(0,1).toUpperCase()+s.substring(1));*/
+
+       ExcleUtils excleUtils = new ExcleUtils();
+       excleUtils.readExcleCarData("D:\\副本GE11互博车造车信息0530.xlsx");
+    }
+
 }
--- src/main/java/poi/pojo/CarBaseInfo.java
+++ src/main/java/poi/pojo/CarBaseInfo.java
@@ -1,4 +1,229 @@
 package poi.pojo;
 
 public class CarBaseInfo {
+    private String vin;
+    private String fccode;
+    private String record_time;
+    private String mes_car_series;
+    private String tem_id;
+    private String iccid;
+    private String imsi;
+    private String msisdn;
+    private String tem_pwd;
+    private String tem_vendor;
+    private String tem_system_version;
+    private String tem_version;
+    private String ihu_id;
+    private String ihu_pwd;
+    private String ihu_vendor;
+    private String ihu_version;
+    private String ihu_system_version;
+    private String series_code_vs;
+    private String mat_code;
+    private String model_code;
+    private String color_code;
+    private String factory_code;
+    private String engine_no;
+    private String dearler_id;
+    private String source;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getFccode() {
+        return fccode;
+    }
+
+    public void setFccode(String fccode) {
+        this.fccode = fccode;
+    }
+
+    public String getRecord_time() {
+        return record_time;
+    }
+
+    public void setRecord_time(String record_time) {
+        this.record_time = record_time;
+    }
+
+    public String getMes_car_series() {
+        return mes_car_series;
+    }
+
+    public void setMes_car_series(String mes_car_series) {
+        this.mes_car_series = mes_car_series;
+    }
+
+    public String getTem_id() {
+        return tem_id;
+    }
+
+    public void setTem_id(String tem_id) {
+        this.tem_id = tem_id;
+    }
+
+    public String getIccid() {
+        return iccid;
+    }
+
+    public void setIccid(String iccid) {
+        this.iccid = iccid;
+    }
+
+    public String getImsi() {
+        return imsi;
+    }
+
+    public void setImsi(String imsi) {
+        this.imsi = imsi;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public String getTem_pwd() {
+        return tem_pwd;
+    }
+
+    public void setTem_pwd(String tem_pwd) {
+        this.tem_pwd = tem_pwd;
+    }
+
+    public String getTem_vendor() {
+        return tem_vendor;
+    }
+
+    public void setTem_vendor(String tem_vendor) {
+        this.tem_vendor = tem_vendor;
+    }
+
+    public String getTem_system_version() {
+        return tem_system_version;
+    }
+
+    public void setTem_system_version(String tem_system_version) {
+        this.tem_system_version = tem_system_version;
+    }
+
+    public String getTem_version() {
+        return tem_version;
+    }
+
+    public void setTem_version(String tem_version) {
+        this.tem_version = tem_version;
+    }
+
+    public String getIhu_id() {
+        return ihu_id;
+    }
+
+    public void setIhu_id(String ihu_id) {
+        this.ihu_id = ihu_id;
+    }
+
+    public String getIhu_pwd() {
+        return ihu_pwd;
+    }
+
+    public void setIhu_pwd(String ihu_pwd) {
+        this.ihu_pwd = ihu_pwd;
+    }
+
+    public String getIhu_vendor() {
+        return ihu_vendor;
+    }
+
+    public void setIhu_vendor(String ihu_vendor) {
+        this.ihu_vendor = ihu_vendor;
+    }
+
+    public String getIhu_version() {
+        return ihu_version;
+    }
+
+    public void setIhu_version(String ihu_version) {
+        this.ihu_version = ihu_version;
+    }
+
+    public String getIhu_system_version() {
+        return ihu_system_version;
+    }
+
+    public void setIhu_system_version(String ihu_system_version) {
+        this.ihu_system_version = ihu_system_version;
+    }
+
+    public String getSeries_code_vs() {
+        return series_code_vs;
+    }
+
+    public void setSeries_code_vs(String series_code_vs) {
+        this.series_code_vs = series_code_vs;
+    }
+
+    public String getMat_code() {
+        return mat_code;
+    }
+
+    public void setMat_code(String mat_code) {
+        this.mat_code = mat_code;
+    }
+
+    public String getModel_code() {
+        return model_code;
+    }
+
+    public void setModel_code(String model_code) {
+        this.model_code = model_code;
+    }
+
+    public String getColor_code() {
+        return color_code;
+    }
+
+    public void setColor_code(String color_code) {
+        this.color_code = color_code;
+    }
+
+    public String getFactory_code() {
+        return factory_code;
+    }
+
+    public void setFactory_code(String factory_code) {
+        this.factory_code = factory_code;
+    }
+
+    public String getEngine_no() {
+        return engine_no;
+    }
+
+    public void setEngine_no(String engine_no) {
+        this.engine_no = engine_no;
+    }
+
+    public String getDearler_id() {
+        return dearler_id;
+    }
+
+    public void setDearler_id(String dearler_id) {
+        this.dearler_id = dearler_id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
 }

213e8137d10b760ee578ab09a2a98a96f1300525
zhangjie <2099487761@qq.com>
2019-07-15 10:24:18 +0800

    正则表达式

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -2,12 +2,18 @@ package test;
 
 import thread.clone.User;
 
+import java.text.SimpleDateFormat;
+
 public class test {
     public static void main(String[] args) {
-        User user = new User();
-        user.setName("zhangjie");
-        user.setAge("17");
-        TransferObj transferObj = new TransferImpl();
-        transferObj.excute(user);
+//        User user = new User();
+//        user.setName("zhangjie");
+//        user.setAge("17");
+//        TransferObj transferObj = new TransferImpl();
+//        transferObj.excute(user);
+        String date = "2019-07-12T06:59:43+08:00";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = dateFormat.format(date);
+        System.out.println(format);
     }
 }

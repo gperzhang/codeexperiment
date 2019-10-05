88d6cf7be45d4931badc52cc17654300b5d5add3
zhangjie <2099487761@qq.com>
2019-10-05 16:55:12 +0800

    测试动态代理

--- src/main/java/pattern/proxy/dynamicproxy/DynamicTest.java
+++ src/main/java/pattern/proxy/dynamicproxy/DynamicTest.java
@@ -4,7 +4,7 @@ public class DynamicTest {
     public static void main(String[] args) {
 
         Person instance = (Person) new DynamicProxy().getInstance(new Son());
-        System.out.println(instance);
+        instance.findLove();
 
     }
 }

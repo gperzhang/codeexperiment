c9c3967aa61a26cca0961fbac5790aa6a0e82c22
zhangjie <2099487761@qq.com>
2019-11-03 11:31:59 +0800

    计算二进制中1的个数

---
+++ src/main/java/algorithm/program/Solution.java
@@ -0,0 +1,31 @@
+package algorithm.program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> countBits(int num){
+        int count =0;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<=num;i++){
+            int result = count2(i);
+            list.add(result);
+        }
+        return list;
+    }
+
+    private int count2(int i) {
+        if(i<2){
+            return i;
+        }if(i%2==0){
+            return count2(i/2);
+        }else {
+            return count2(i-1)+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new Solution().countBits(5);
+        list.stream().forEach(val-> System.out.println(val));
+    }
+}
